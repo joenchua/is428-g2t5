f567b421c235ee04a080e9427b165961cb0d003b
Vera Chua <vera.chua.2019@scis.smu.edu.sg>
2021-11-04 15:59:08 +0800

    Bumpchart done with countries filter and packages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 // import data from './data';
 import { Layout } from 'antd';
 import View1 from './views/View1';
+import BumpChart from './views/BumpChartView';
 
 import * as d3 from "d3";
 import "./App.css";
@@ -24,10 +25,13 @@ function App() {
   return (
     <div>
       <div className='title'>Visualising Olympics Data</div>
-      <Layout style={{ height: 920 }}>
+      <Layout style={{ height: 2000 }}>
           <Layout>
             <Content style={{ height: 200, width: 300 }}>
               <View1 data={data}/>
+            </Content>
+            <Content style={{ height: 1000, width: 2000 }}>
+              <BumpChart data={data}/>
             </Content>
           </Layout>
       </Layout>
