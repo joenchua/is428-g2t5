5eee96319708c84e169060f0c12b6bd131db2365
ShermzLim <sherminlimhuian@gmail.com>
2021-11-06 22:56:09 +0800

    completed top 5 countries bar chart

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { csv } from 'd3';
 
-// import data from './data';
 import { Layout } from 'antd';
 import View1 from './views/View1';
+import View5 from './views/View5';
+// import data from '/athlete_events.csv';
 
-import * as d3 from "d3";
+// import * as d3 from "d3";
 import "./App.css";
 
 const { Content } = Layout;
 
-function App() {
-  const [data, setData] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  React.useEffect(() => {
-    d3.csv("/athlete_events.csv").then((d) => {
-      setData(d);
-      setLoading(false);
-    });
-    return () => undefined;
-  }, []);
+  // useEffect(() => {
+  //   csv("athlete_events.csv").then(data => {
+  //     setData(data);
+  //     setLoading(false);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    const getClassDetails = async () => {
+      try {
+        const data = await csv("athlete_events.csv");
+        if (data.length > 0) {
+          setData(data);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+        return () => undefined;
+    };
+    getClassDetails();
+  },[])
 
   return (
     <div>
       <div className='title'>Visualising Olympics Data</div>
-      <Layout style={{ height: 920 }}>
-          <Layout>
-            <Content style={{ height: 200, width: 300 }}>
-              <View1 data={data}/>
-            </Content>
-          </Layout>
-      </Layout>
+      {loading && <div>loading</div>}
+      {!loading && 
+        <Layout style={{ height: 920 }}>
+            <Layout>
+              <Content style={{ height: 200, width: 300 }}>
+                <View1 data={data}/>
+              </Content>
+            </Layout>
+            <Layout>
+              <Content style={{ height: 500, width: 750 }}>
+                <View5 data={data}/>
+              </Content>
+            </Layout>
+        </Layout>
+      }
 </div>
   );
 }
---
+++ src/views/View5/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './view5.css';
+import BarChart from '../../charts/BarChart';
+
+export default class View5 extends Component {
+    render() {
+        const {data} = this.props;
+        const width = 900;
+        const height = 500;
+        return (
+            <div id='view5' className='pane'>
+                <div className='header'>Top 5 Countries </div>
+                <BarChart data={data} width={width} height={height} />
+            </div>
+        )
+    }
+}
